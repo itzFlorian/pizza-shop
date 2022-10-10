75053bdf76a8e5f6755861d7f67615701f42d2f3
Florian <f.mewes87@gmail.com>
2022-10-10 20:01:52 +0200

    Main läuft shoppingcart macht noch ärger.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Contact from "./components/Contact"
 import Main from "./components/Main"
 import ShoppingCart from "./components/ShoppingCart"
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [shoppingCart, setShoppingCart] = useState([])
+
   return (
     <div className="App">
       <Header/>
       <Routes>
         <Route path="/" element={<Navigate to="/main"/>} />
-        <Route path="/main" element={<Main/>} />
-        <Route path="/shopping-cart" element={<ShoppingCart/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/main" element={<Main shoppingCart={shoppingCart} setShoppingCart={setShoppingCart} />} />
+        <Route path="/shopping-cart" element={<ShoppingCart shoppingCart={shoppingCart} setShoppingCart={setShoppingCart} />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Navigate to="/main" />} />
       </Routes>
     </div>
