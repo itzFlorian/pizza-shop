74c685a5e9ea4fdb148f24ce65d21a4cdc395da6
Florian <f.mewes87@gmail.com>
2022-10-11 12:49:47 +0200

    conditional rendering für shoppingcart implementiert

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,30 +1,30 @@
 import CartContext from "../context/CartContext";
-import { useContext, useSate } from "react";
+import { useContext} from "react";
 
 
 const ShoppingCart = () => {
-  const [cart, setCart] = useContext(CartContext)
+  const [cart] = useContext(CartContext)
   const prices = cart.map(item =>{
     return item.price[item.size]
   })
-  console.log(prices);
-  return (
+   return  cart.length > 0 && (    
     <>
       <h1>Here is your Order</h1>
       {cart.map(item => {
         return (
           <div className="cart-container">
-            <p>Name: {item.pizza}</p>
+            <p>{item.pizza}</p>
             <p>size: {item.size}</p>
             <p>price: {`${item.price[item.size]} €`}</p>
+            <img className="pizza-img" src={item.img} alt="pizza" />
           </div>
         )
       })}
       <h2>Total: {prices.reduce((num1, num2)=>{
         return (num1+num2)
-      })} €</h2>
+      })} €</h2>      
     </>
-  );
+  )
 };
 
 export default ShoppingCart;
