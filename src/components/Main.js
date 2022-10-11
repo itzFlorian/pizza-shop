6d5674aa4a9332c8533bf182fbb7670af8e5063a
Florian <f.mewes87@gmail.com>
2022-10-11 14:54:24 +0200

    Search funktioniert ist aber mit sicherheit einfacher machbar.

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,10 @@ import CartContext from "../context/CartContext";
 import Search from "./Search"
 
 const Main = () => {
+  const [searchList, setSearchList] = useState([])
   const [pizzas, setPizzas] = useState(pizzaList)
   const [cart, setCart] = useContext(CartContext)
+  const [input, setInput] = useState("")
 
   const sizeHandler = (event, pizza) => {
     const size = event.target.value
@@ -23,9 +25,9 @@ const Main = () => {
   }
   
 
-  return (
+  return input === "" ? (
     <>
-      <Search/>      
+      <Search pizzas={pizzas} searchList={searchList} setSearchList={setSearchList} input={input} setInput={setInput}/>      
       {pizzas.map(pizza=>{
         return (
           <div className="pizza-container">
@@ -45,9 +47,32 @@ const Main = () => {
               <button type="button" onClick={(event) => cartHandler(event, pizza)} >Add to Cart</button>
             </div>
           </div>
-       ) })}        
+       ) })}      
     </>
-  );
+  ) : (    
+  <>
+    <Search pizzas={pizzas} searchList={searchList} setSearchList={setSearchList} input={input} setInput={setInput}/>      
+    {searchList.map(pizza=>{
+      return (
+        <div className="pizza-container">
+          <div>
+            <h2>{pizza.pizza}</h2>
+            <img className="pizza-img" src={pizza.img} alt="pizza" />
+          </div>
+          <div className="pizza-element">
+            <select onChange={(event)=> sizeHandler(event, pizza)} id="size">
+              <option value="m">Medium</option>
+              <option value="s">Small</option>
+              <option value="l">Large</option>
+            </select>
+            <p>
+              {`${pizza.price[pizza.size]} €`}
+            </p>
+            <button type="button" onClick={(event) => cartHandler(event, pizza)} >Add to Cart</button>
+          </div>
+        </div>
+     ) })}      
+  </>)
 };
 
 export default Main;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,19 @@
-const Search = () => {
+const Search = ({setSearchList, pizzas, setInput, input}) => {
+  const changeHandler = (event) =>{
+    setInput(event.target.value)    
+  }
+
+const clickHandler = (event)=>{
+  event.preventDefault()
+  const newList = pizzas.filter(pizza => pizza.pizza.toLowerCase().includes(input.toLowerCase()))
+  setSearchList(newList)  
+}
   return (
     <div className="search">
-      <label htmlFor="search">Search for your pizza: </label>
-      <input type="text" id="search" />
+      <form onChange={event => clickHandler(event)}>
+        <label htmlFor="search">Search for your pizza: </label>
+        <input type="text" id="search" value={input} onChange={event => changeHandler(event)}/>
+      </form>
     </div>
   );
 };
